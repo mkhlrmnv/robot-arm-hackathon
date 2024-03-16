3576bea7e156b35b55291141af67485ddb9b8480
Mikhail Romanov <mikhail.romanov@aalto.fi>
2024-03-17 00:38:25 +0200

    tried fixing

--- hand_tracking/main/main.ino
+++ hand_tracking/main/main.ino
@@ -1,10 +1,13 @@
 void setup() {
   Serial.begin(9600);
+  Serial.println("Arduino initialized");
 }
 
 void loop() {
   if (Serial.available() > 0) {
     String data = Serial.readStringUntil('\n');
+    Serial.println("Received data: " + data);
+    
     int commaIndex = data.indexOf(',');
     if (commaIndex != -1) {
       String x_str = data.substring(0, commaIndex);
@@ -13,11 +16,12 @@ void loop() {
       int x = x_str.toInt();
       int y = y_str.toInt();
 
-      // Print the received coordinates
       Serial.print("Received coordinates: ");
       Serial.print(x);
       Serial.print(", ");
       Serial.println(y);
+    } else {
+      Serial.println("Invalid data format");
     }
   }
 }
