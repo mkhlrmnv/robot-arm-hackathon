b6c6d33fbf3c760559452bd502ee3791987ef89b
Mikhail Romanov <mikhail.romanov@aalto.fi>
2024-03-16 20:34:21 +0200

    made tests for angles and real values

--- hand_tracking/test.py
+++ hand_tracking/test.py
@@ -10,13 +10,13 @@ class TestRobotArmSimulation(unittest.TestCase):
         x, y = 44, 44
         theta1, theta2 = self.robot_arm.calculate_angles(x, y)
         self.assertAlmostEqual(theta1, 0, places=2)
-        self.assertAlmostEqual(theta2, 0, places=2)
+        self.assertAlmostEqual(theta2, 1.5707963267948966, places=2)
 
         # Test when x and y are outside valid range
-        x, y = 100, 100
+        x, y = 32, -42
         theta1, theta2 = self.robot_arm.calculate_angles(x, y)
-        self.assertAlmostEqual(theta1, 0, places=2)
-        self.assertAlmostEqual(theta2, 0, places=2)
+        self.assertAlmostEqual(theta1, 0.007554090747718956, places=2)
+        self.assertAlmostEqual(theta2, -1.8545473921962716, places=2)
 
     def test_get_real(self):
         # Test when x and y are within valid range
@@ -26,10 +26,10 @@ class TestRobotArmSimulation(unittest.TestCase):
         self.assertAlmostEqual(real_y, 44, places=2)
 
         # Test when x and y are outside valid range
-        x, y = 1.5, 1.5
+        x, y = 0.75, 0.1
         real_x, real_y = self.robot_arm.get_real(x, y)
-        self.assertAlmostEqual(real_x, 132, places=2)
-        self.assertAlmostEqual(real_y, 0, places=2)
+        self.assertAlmostEqual(real_x, 44, places=2)
+        self.assertAlmostEqual(real_y, 79.2, places=2)
 
 if __name__ == '__main__':
     unittest.main()
