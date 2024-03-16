b753666c05d7d4444c592abfb22bd3536503cda7
Mikhail Romanov <mikhail.romanov@aalto.fi>
2024-03-16 21:47:18 +0200

    made class:)

--- hand_tracking/main.py
+++ hand_tracking/main.py
@@ -1,19 +1,21 @@
-# from simulation import RobotArmSimulation
-from tracker import HandTracker
 import serial
 import time
+from tracker import HandTracker
 
-ser = serial.Serial('/dev/cu.usbserial-1110', 9600)  # Change 'COM3' to the appropriate port
+class CoordinateCalculator:
+    def __init__(self):
+        pass
 
-def send_coordinates_to_arduino(ser):
-    x, y = HandTracker.getPalmCoords()
-    print(f"x: {x}, y: {y}")
-    ser.write(f"{x},{y}\n".encode())
-    print(f"Sent coordinates: {x}, {y}")
+    def send_coordinates_to_arduino(self, ser):
+        x, y = HandTracker.getPalmCoords()
+        ser.write(f"{x},{y}\n".encode())
+        print(f"Sent coordinates: {x}, {y}")
 
-time.sleep(2)  # Wait for the serial connection to establish
-calculator = HandTracker.getPalmCoords()
+if __name__ == "__main__":
+    ser = serial.Serial('/dev/cu.usbserial-110', 9600)  # Change 'COM3' to the appropriate port
+    time.sleep(2)  # Wait for the serial connection to establish
+    calculator = CoordinateCalculator()
 
-while True:
-    calculator.send_coordinates_to_arduino(ser)
-    time.sleep(1)  # Adjust the delay as needed
+    while True:
+        calculator.send_coordinates_to_arduino(ser)
+        time.sleep(1)  # Adjust the delay as needed
