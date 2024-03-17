d8fa0bd19e4def8a9e6cb278214e8857af57cd7f
Mikhail Romanov <mikhail.romanov@aalto.fi>
2024-03-17 03:55:39 +0200

    updated code that takes cords from python and displays them

--- hand_tracking/main/main.ino
+++ hand_tracking/main/main.ino
@@ -1,31 +1,77 @@
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SH1107.h>
+#include "OLED_Driver.h"
+#include "GUI_paint.h"
+#include "DEV_Config.h"
+#include "Debug.h"
+#include "ImageData.h"
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 128 // OLED display height, in pixels
-
-Adafruit_SH1107 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+String cmd;
+int i = 100;
 
 void setup() {
-  Serial.begin(9600);
-  if (!display.begin(SH1107_ADDR_DEFAULT, SCREEN_HEIGHT)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;);
+  System_Init();
+  Serial.print(F("OLED_Init()...\r\n"));
+  OLED_1in5_Init();
+  Driver_Delay_ms(500); 
+  OLED_1in5_Clear();  
+  
+  //0.Create a new image cache
+  UBYTE *BlackImage;
+  UWORD Imagesize = ((OLED_1in5_WIDTH%2==0)? (OLED_1in5_WIDTH/2): (OLED_1in5_WIDTH/2+1)) * OLED_1in5_HEIGHT;
+  if((BlackImage = (UBYTE *)malloc(Imagesize/8)) == NULL) { //No enough memory
+      Serial.print("Failed to apply for black memory...\r\n");
+      return -1;
   }
-  delay(2000);
-  display.clearDisplay();
+  Serial.print("Paint_NewImage\r\n");
+  Paint_NewImage(BlackImage, OLED_1in5_WIDTH/4, OLED_1in5_HEIGHT/2, 270, BLACK);  
+  Paint_SetScale(16);
+
+  //1.Select Image
+  Paint_SelectImage(BlackImage);
+  Paint_Clear(BLACK);
+  Driver_Delay_ms(500); 
+
+while (1) {
+    i++;
+
+    while (Serial.available() == 0){
+    }
+    cmd = Serial.readStringUntil('\r');
+
+    Serial.print("Received command: ");
+    Serial.println(cmd);
+
+    char* token = strtok(cmd.c_str(), ";"); // Get the first token
+    int x = atoi(token); // Convert token to float for x
+
+    token = strtok(NULL, ";"); // Get the second token
+    int y = atoi(token); // Convert token to float for y
+
+    token = strtok(NULL, ";"); // Get the third token
+    int open = atoi(token); // Convert token to float for open
+
+    Paint_ClearWindows(10, 0, 64, 30, BLACK);
+    OLED_1in5_Clear(); 
+
+    char numberString[20]; // Increased size to accommodate larger numbers
+    snprintf(numberString, sizeof(numberString), "x: %d", x); // Format x value as float with 2 decimal places
+
+    char numberString2[20]; // Increased size to accommodate larger numbers
+    snprintf(numberString2, sizeof(numberString2), "y: %d", y); // Format y value as float with 2 decimal places
+
+    char numberString3[20]; // Increased size to accommodate larger numbers
+    snprintf(numberString3, sizeof(numberString3), "z: %d", open); // Format open value as float with 2 decimal places
+    
+    Paint_DrawString_EN(10, 0, numberString, &Font12, WHITE, WHITE);
+    Paint_DrawString_EN(10, 10, numberString2, &Font12, WHITE, WHITE);
+    Paint_DrawString_EN(10, 20, numberString3, &Font12, WHITE, WHITE);
+    OLED_1in5_Display_Part(BlackImage, 0, 0, 32, 64); 
+
+    Driver_Delay_ms(100);  
+    
+    // OLED_1in5_Clear();  
+  }   
 }
 
 void loop() {
-  display.clearDisplay();
-  for (int i = 0; i <= 1000; i++) {
-    display.setTextSize(1);
-    display.setTextColor(SSD1306_WHITE);
-    display.setCursor(0, 0);
-    display.print("Count: ");
-    display.println(i);
-    display.display();
-    delay(500); // Adjust delay as needed
-  }
+
 }
