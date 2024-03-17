19d9573126af5e9baf319adfae5e6ee3c1fb18c1
Mikhail Romanov <mikhail.romanov@aalto.fi>
2024-03-17 09:35:15 +0200

    added comments

--- hand_tracking/tracker.py
+++ hand_tracking/tracker.py
@@ -1,9 +1,28 @@
 import mediapipe as mp
 import cv2
 import numpy as np
+"""
+Imports all necessary libraries
+Mediapipe is Google's ML that is being used to detect hands and fingers
+cv2 is library for video capture
+numpy is library for all math
+"""
+
 
 class HandTracker:
+    """
+    Tracks hand's and gives some parameters back depending on the function
+    """
+
     def __init__(self):
+        """
+        Initializes everything up like:
+            - like opening webcamera with cv2
+            - initializes Mediapipe class
+
+            :return: returns nothing
+        """
+
         # Initialize MediaPipe Hands
         self.mp_drawing = mp.solutions.drawing_utils
         self.mp_hands = mp.solutions.hands
@@ -12,24 +31,38 @@ class HandTracker:
         self.cap = cv2.VideoCapture(0)
         self.width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         self.height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        # Arm setupq
-        self.arm_length = 150
-        self.arm_thickness = 10
-        self.arm_color = (0, 255, 0)
-        self.arm_center = (self.width // 2, self.height // 2)
-        self.angle1 = 0
-        self.angle2 = 0
 
     def dist(self, list):
+        """
+        Calculates total distance between list of points (x, y)
+
+        :param list: list of hand landmarks => list[landmark]
+        :return: returns total distance between all points / landmarks
+        """
+
+        # variable for result
         res = 0
+
+        # goes through all list
         for i in range(len(list) - 1):
+            # dist between p1 and p2 is sqrt((p1.x - p2.x)^2 + (p1.y - p2.y)^2)
             res += np.sqrt(np.power(list[i].x - list[i + 1].x, 2) + np.power(list[i].y - list[i + 1].y, 2))
         return res
 
     def getPalmCoords(self):
+        """
+        Function takes current view from webcamera and returns palm coords and distance between index and thumb fingers
+
+        :return: Returns list[tuple(thumb x, thumb y), tuple(dist between index and thumb, total len of index finger)]
+        """
+
+        # reads current view from camera
         ret, frame = self.cap.read()
         if not ret:
             return None
+        
+        # next 3 steps are done cause cv2 and mp processes pictures in different color scales
+        # cv2 is using BRG and mp RGB
         
         # BGR 2 RGB
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -40,7 +73,7 @@ class HandTracker:
         # Set flag
         image.flags.writeable = False
 
-        # Detections
+        # Passes pic to mp for detection
         results = self.hands.process(image)
 
         # Set flag to true
@@ -50,25 +83,37 @@ class HandTracker:
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         cv2.imshow(":)", image)
 
+        # if there are any detection
         if results.multi_hand_landmarks:
+            
+            #for all detection
             for hand in results.multi_hand_landmarks:
-                # Assuming you want coordinates of the first landmark
+                # Max amount of hands to be detected is 1, so we take first detection, because can't be more:)
+                
+                # Taking palm coords
                 palm_coords = hand.landmark[0].x, hand.landmark[0].y
 
+                # takes end of index and thumb fingers and passes it to dist function
                 fingerList = [hand.landmark[8], hand.landmark[4]]
                 fingerDist = self.dist(fingerList)
-                # print(f"finger dist: {fingerDist}")
 
+                # takes all points of index finger and passes them to dist function
                 indexList = [hand.landmark[5], hand.landmark[6], hand.landmark[7], hand.landmark[8]]
                 indexLen = self.dist(indexList)
-                # print(f"index len : {indexLen}")
-
+                
+                # makes tuple
                 dists = fingerDist, indexLen
 
+                #returns list all
                 return [palm_coords, dists]
+            
+        # base case if there wan't any detections
         return [(0.5, 0), (1, 1)]
 
     def close(self):
+        """
+        closes webcam and all windows
+        """
         self.cap.release()
         cv2.destroyAllWindows()
 
